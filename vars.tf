35d9c63f23125b9ad7f8ad1ca4d5c43dc97e8c96
KARTHIK CHARY <34944541+karthikchary@users.noreply.github.com>
2024-05-12 19:19:41 +0530

    Create vars.tf
    added vars file

---
+++ vars.tf
@@ -0,0 +1,15 @@
+variable "region" {
+  default = "us-east-1"
+}
+
+variable "httpd_port" {
+  default = "80"
+}
+
+variable "ssh_port" {
+  default = "22"
+}
+
+variable "instance_type" {
+  default = "t2.micro"
+}
