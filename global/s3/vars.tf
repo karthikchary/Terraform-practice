f983f61c8b4aa21800671e31af16ad23e3685314
KARTHIK CHARY <34944541+karthikchary@users.noreply.github.com>
2024-05-13 18:42:31 +0530

    update the terraform architecture

--- global/s3/vars.tf
+++ global/s3/vars.tf
@@ -2,18 +2,6 @@ variable "region" {
   default = "us-east-1"
 }
 
-variable "httpd_port" {
-  default = "80"
-}
-
-variable "ssh_port" {
-  default = "22"
-}
-
-variable "instance_type" {
-  default = "t2.micro"
-}
-
 variable "bucket_name" {
   description = "The name of the S3 bucket. Must be globally unique."
   default     = "terraform-state-my-bucket"
--- stage/output.tf
+++ stage/output.tf
@@ -5,6 +5,3 @@
 output "elb_dns_name" {
   value = "${aws_elb.example.dns_name}"
 }
-output "s3_bucket_arn" {
-  value = "${aws_s3_bucket.terraform_state.arn}"
-}
