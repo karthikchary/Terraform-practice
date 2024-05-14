067e98ce9f8063527fc96e087da218285a98c02c
KARTHIK CHARY <34944541+karthikchary@users.noreply.github.com>
2024-05-14 08:55:55 +0530

    added apache playbook

--- EC2/main.tf
+++ EC2/main.tf
@@ -25,6 +25,54 @@ resource "aws_instance" "example" {
     Name = "webserver",
     owner = "karthik"
   }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
+  }
+  provisioner "local-exec" {
+    command = "touch dynamic_inventory.ini"
+  }
+
+  provisioner "remote-exec" {
+    inline = [
+      "echo 'EC2 instance is ready.'"
+    ]
+
+    connection {
+      type        = "ssh"
+      host        = self.public_ip
+      user        = "ubuntu"
+      private_key = tls_private_key.example.private_key_pem
+    }
+
+  }
+}
+
+data "template_file" "inventory" {
+  template = <<-EOT
+    [ec2_instances]
+    ${aws_instance.example.public_ip} ansible_user=ubuntu ansible_private_key_file=${path.module}/${var.key_name}
+    EOT
+}
+
+resource "local_file" "dynamic_inventory" {
+  depends_on = [aws_instance.example]
+
+  filename = "dynamic_inventory.ini"
+  content  = data.template_file.inventory.rendered
+
+  provisioner "local-exec" {
+    command = "chmod 400 ${local_file.dynamic_inventory.filename}"
+  }
+}
+
+resource "null_resource" "run_ansible" {
+  depends_on = [local_file.dynamic_inventory]
+
+  provisioner "local-exec" {
+    command = "ansible-playbook -i dynamic_inventory.ini apache-playbook.yml"
+    working_dir = path.module
+  }
 }
 
 resource "aws_instance" "jenkins" {
@@ -36,5 +84,9 @@ resource "aws_instance" "jenkins" {
   tags = {
     Name = "Jenkins",
     owner = "karthik"
+  }
+  root_block_device {
+    volume_size = 30
+    volume_type = "gp2"
   }
 }
